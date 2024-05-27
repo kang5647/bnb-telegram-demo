7724d69f0bbbf50e9b7e0dc5017e16d5c6fb2aa5
kang5647 <kang5647@hotmail.com>
2024-05-27 15:06:54 +0800

    Updates

---
+++ assets/masterPasswordDescription-UNSF76DN-BR4EXVky.js
@@ -0,0 +1,22 @@
+import{w as c}from"./chunk-KXKRH74J-CvUHXByS.js";import{i as n}from"./chunk-QDW3HRTT-h_gfYRYM.js";import{N as s}from"./chunk-DTK23SGK-DUEWRECq.js";import{Y as m,o as e}from"./index-DZCF-iRb.js";import{u as l}from"./useTranslation-B3ncPlRf.js";import{C as i,T as p}from"./index-6mmh9ZQD.js";import"./TextArea-JzyJVH2a.js";import"./colors-DjHTqeq_.js";import"./index-C7nvuSOJ.js";var u=`.master-password-description-container {
+  display: flex;
+  flex-direction: column;
+}
+.about-mp-container {
+  padding: 0 18px;
+}
+.about-mp-container .space-container {
+  width: 100%;
+  padding: 20px 0;
+}
+.about-mp-container .space-container .master-title {
+  text-align: center;
+}
+.about-mp-container .space-container .ant-typography {
+  color: var(--text-color) !important;
+}
+.about-mp-container .space-container .content {
+  font-size: 14px;
+  color: var(--secondary-text-color) !important;
+}
+`,d=()=>{let{Title:a,Paragraph:o}=p,r=m(),{t}=l();return e.createElement("div",{className:"master-password-description-container height-100"},e.createElement(c,null,t("account.about_master_password")),e.createElement("div",{className:"scroll-content"},e.createElement("style",null,u),e.createElement("div",{className:"about-mp-container"},e.createElement(n,{className:"icon-navigation-back",name:"circle_back",onClick:()=>r(-1)}),e.createElement(i,{direction:"vertical",className:"space-container"},e.createElement(a,{level:5,className:"title"},t("account.about_master_password_t1")),e.createElement(o,{className:"content"},t("account.about_master_password_p1")),e.createElement(a,{level:5,className:"title"},t("account.about_master_password_t2")),e.createElement(o,{className:"content"},t("account.about_master_password_p2")),e.createElement(a,{level:5,className:"title"},t("account.about_master_password_t3")),e.createElement(o,{className:"content"},t("account.about_master_password_p3"))))),e.createElement(s,{className:"footer-box-v2"}))},y=d;export{y as default};
